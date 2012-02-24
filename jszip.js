9c70a3c9ee8a330cbdebfb37969a88120593d1c2
David Duponchel <d.duponchel@gmail.com>
2012-02-24 18:27:05 +0100

    Fix errors : console.log and global variables.

--- jszip.js
+++ jszip.js
@@ -584,7 +584,7 @@ JSZip.prototype = (function ()
       utf8decode : function (utftext) {
          var string = "";
          var i = 0;
-         var c = c1 = c2 = 0;
+         var c = 0, c1 = 0, c2 = 0, c3 = 0;
 
          while ( i < utftext.length ) {
 
