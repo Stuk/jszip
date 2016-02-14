431a1ce8ea9ea56065e251c51ac89e2041c3f35b
David Duponchel <d.duponchel@gmail.com>
2016-02-14 22:14:09 +0100

    Remove the gzip reporter.
    It uses a dependency which requires node >=0.12. On node 0.10 (used in the
    travis builds) this reporter break the buid with a
    
        Object #<Object> has no method 'gzipSync'
    
    This reporter needs the `--verbose` flag on grunt to display its
    result, this commit won't change anything to the default output.

--- Gruntfile.js
+++ Gruntfile.js
@@ -115,7 +115,6 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        report: 'gzip',
         mangle: true,
         preserveComments: false,
         banner : grunt.file.read('lib/license_header.js')
