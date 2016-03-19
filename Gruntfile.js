5433655ddd33b3566955dd80aede9a9d4e45cb26
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2016-03-19 01:54:14 +0100

    Improve browser testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,31 +5,51 @@ module.exports = function(grunt) {
       platform: "OS X 10.8",
       version: "6"
   }, {
+      browserName: "iphone",
+      platform: "OS X 10.10",
+      version: "9.2"
+  }, {
       browserName: "android",
       platform: "Linux",
       version: "4.0"
   }, {
+      browserName: "android",
+      platform: "Linux",
+      version: "4.4"
+  }, {
+      browserName: "android",
+      platform: "Linux",
+      version: "5.1"
+  }, {
       browserName: "firefox",
-      platform: "XP"
+      platform: "Windows 10"
   }, {
       browserName: "chrome",
-      platform: "XP"
+      platform: "Windows 10"
   }, {
       browserName: "internet explorer",
-      platform: "WIN8",
-      version: "10"
-  }, {
-      browserName: "internet explorer",
-      platform: "VISTA",
-      version: "9"
+      platform: "XP",
+      version: "7"
   }, {
       browserName: "internet explorer",
       platform: "Windows 7",
       version: "8"
   }, {
       browserName: "internet explorer",
-      platform: "XP",
-      version: "7"
+      platform: "Windows 7",
+      version: "9"
+  }, {
+      browserName: "internet explorer",
+      platform: "Windows 8",
+      version: "10"
+  }, {
+      browserName: "internet explorer",
+      platform: "Windows 10",
+      version: "11"
+  }, {
+      browserName: "edge",
+      platform: "Windows 10",
+      version: "13.10586"
   }, {
       browserName: "opera",
       platform: "Windows 2008",
@@ -38,6 +58,18 @@ module.exports = function(grunt) {
       browserName: "safari",
       platform: "OS X 10.8",
       version: "6"
+  }, {
+      browserName: "safari",
+      platform: "OS X 10.9",
+      version: "7"
+  }, {
+      browserName: "safari",
+      platform: "OS X 10.10",
+      version: "8"
+  }, {
+      browserName: "safari",
+      platform: "OS X 10.11",
+      version: "9"
   }];
 
   var tags = [];
