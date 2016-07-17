a750ccd178020757bf2e86cac75a5cc9fba10c9a
David Duponchel <d.duponchel@gmail.com>
2016-07-17 15:10:08 +0200

    [backport] Fix unit tests.
    The iphone 6 isn't supported anymore by saucelabs, I replaced it by the
    iphone 7.
    
    This is a backport of 5b04dfc.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,11 @@
 /*jshint node: true */
 module.exports = function(grunt) {
+  // see https://saucelabs.com/rest/v1/info/browsers/webdriver
   var browsers = [{
       browserName: "iphone",
-      platform: "OS X 10.8",
-      version: "6"
+      version: "7.0"
   }, {
       browserName: "iphone",
-      platform: "OS X 10.10",
       version: "9.2"
   }, {
       browserName: "android",
