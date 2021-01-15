7b9f10f0b0433bcea4c8a3f85f99d33a22d9bfd8
ash <ashfixit@gmail.com>
2021-01-14 19:34:14 -0500

    Update dist's

--- dist/jszip.js
+++ dist/jszip.js
@@ -558,17 +558,17 @@ var generateZipParts = function(streamInfo, streamedContent, streamingEnded, off
     // @see http://www.delorie.com/djgpp/doc/rbinter/it/65/16.html
     // @see http://www.delorie.com/djgpp/doc/rbinter/it/66/16.html
 
-    dosTime = date.getUTCHours();
+    dosTime = date.getHours();
     dosTime = dosTime << 6;
-    dosTime = dosTime | date.getUTCMinutes();
+    dosTime = dosTime | date.getMinutes();
     dosTime = dosTime << 5;
-    dosTime = dosTime | date.getUTCSeconds() / 2;
+    dosTime = dosTime | date.getSeconds() / 2;
 
-    dosDate = date.getUTCFullYear() - 1980;
+    dosDate = date.getFullYear() - 1980;
     dosDate = dosDate << 4;
-    dosDate = dosDate | (date.getUTCMonth() + 1);
+    dosDate = dosDate | (date.getMonth() + 1);
     dosDate = dosDate << 5;
-    dosDate = dosDate | date.getUTCDate();
+    dosDate = dosDate | date.getDate();
 
     if (useUTF8ForFileName) {
         // set the unicode path extra field. unzip needs at least one extra
@@ -1897,13 +1897,13 @@ DataReader.prototype = {
      */
     readDate: function() {
         var dostime = this.readInt(4);
-        return new Date(Date.UTC(
+        return new Date(
         ((dostime >> 25) & 0x7f) + 1980, // year
         ((dostime >> 21) & 0x0f) - 1, // month
         (dostime >> 16) & 0x1f, // day
         (dostime >> 11) & 0x1f, // hour
         (dostime >> 5) & 0x3f, // minute
-        (dostime & 0x1f) << 1)); // second
+        (dostime & 0x1f) << 1); // second
     }
 };
 module.exports = DataReader;
