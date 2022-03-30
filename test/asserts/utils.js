459ff799478c1d04f5ef4eaa13d834fda036a2fd
Stuart Knightley <git@website.stuartk.com>
2022-03-30 11:42:13 -0700

    Add tests for utils that remove leading slash

---
+++ test/asserts/utils.js
@@ -0,0 +1,20 @@
+/* global QUnit,JSZip,JSZipTestUtils */
+'use strict';
+
+var utils = require("../../lib/utils");
+
+QUnit.module("utils");
+
+QUnit.test("Paths are resolved correctly", function (assert) {
+    assert.strictEqual(utils.resolve("root\\a\\b"), "root/a/b");
+    assert.strictEqual(utils.resolve("root/a/b"), "root/a/b");
+    assert.strictEqual(utils.resolve("root/a/.."), "root");
+    assert.strictEqual(utils.resolve("root/a/../b"), "root/b");
+    assert.strictEqual(utils.resolve("root/a/./b"), "root/a/b");
+    assert.strictEqual(utils.resolve("root/../../../"), "");
+    assert.strictEqual(utils.resolve("////"), "");
+    assert.strictEqual(utils.resolve("/a/b/c"), "a/b/c");
+    assert.strictEqual(utils.resolve("a/b/c/"), "a/b/c/");
+    assert.strictEqual(utils.resolve("../../../../../a"), "a");
+    assert.strictEqual(utils.resolve("../app.js"), "app.js");
+});
